8a652a1331c608524e4c7258e25993326f5da1ef
Malarnithi <malarnithivelusamy21@gmail.com>
2023-09-27 06:14:15 +0530

    push

---
+++ script.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const openFormButton = document.getElementById("openForm");
+    const closeFormButton = document.getElementById("closeForm");
+    const enquiryForm = document.getElementById("enquiryForm");
+
+    openFormButton.addEventListener("click", function () {
+        enquiryForm.style.display = "block";
+        setTimeout(function () {
+            enquiryForm.style.right = "0";
+        }, 10);
+    });
+
+    closeFormButton.addEventListener("click", function () {
+        enquiryForm.style.right = "-350px"; // Adjust the distance from the right side
+        setTimeout(function () {
+            enquiryForm.style.display = "none";
+        }, 300);
+    });
+});
